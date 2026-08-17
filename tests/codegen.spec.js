5f5a3e2e58cadb64d45e9939d027179380d98257
tariqusman889 <tariqusman889@gmail.com>
2026-08-17 21:22:39 +0500

    my 3rd commit

--- tests/codegen.spec.js
+++ tests/codegen.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
+  await page.goto('https://www.saucedemo.com/////');
   await page.locator('[data-test="username"]').fill('standard_user');
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
